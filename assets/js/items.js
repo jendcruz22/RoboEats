3a247cd2000b7b0116c581bcfd95f506567f6c86
Jenny Dcruz <jendcruz23@gmail.com>
2021-12-07 12:45:22 -0500

    fixed: redundancy in items.js

--- assets/js/items.js
+++ assets/js/items.js
@@ -3,33 +3,14 @@ window.onload = ItemDisplay;
 function ItemDisplay(){
     jQuery(document).ready(function () {
         // Hide all item descriptions
-        $("#item1Description").hide();
-        $("#item2Description").hide();
-        $("#item3Description").hide();
-        $("#item4Description").hide();
-        $("#item5Description").hide();
-        $("#item6Description").hide();
+        $(".menu-description").hide();
         $("#btm-Menu").hide();
 
         // Display DESKTOP item 1 description ONLY
         $("#desktop-item1").click(function(){
             $("#item1Description").show();
             $("#btm-Menu").show();
-
-            $("#desktop-item1").hide();
-            $("#desktop-item2").hide();
-            $("#desktop-item3").hide();
-            $("#desktop-item4").hide();
-            $("#desktop-item5").hide();
-            $("#desktop-item6").hide();
-
-            $("#mobile-item1").hide();
-            $("#mobile-item2").hide();
-            $("#mobile-item3").hide();
-            $("#mobile-item4").hide();
-            $("#mobile-item5").hide();
-            $("#mobile-item6").hide();
-
+            $(".menu-item").hide();
             $("#btm-mainMenu").hide();
         })
 
@@ -37,21 +18,7 @@ function ItemDisplay(){
         $("#desktop-item2").click(function(){
             $("#item2Description").show();
             $("#btm-Menu").show();
-
-            $("#desktop-item1").hide();
-            $("#desktop-item2").hide();
-            $("#desktop-item3").hide();
-            $("#desktop-item4").hide();
-            $("#desktop-item5").hide();
-            $("#desktop-item6").hide();
-
-            $("#mobile-item1").hide();
-            $("#mobile-item2").hide();
-            $("#mobile-item3").hide();
-            $("#mobile-item4").hide();
-            $("#mobile-item5").hide();
-            $("#mobile-item6").hide();
-
+            $(".menu-item").hide();
             $("#btm-mainMenu").hide();
         })
 
@@ -59,21 +26,7 @@ function ItemDisplay(){
         $("#desktop-item3").click(function(){
             $("#item3Description").show();
             $("#btm-Menu").show();
-
-            $("#desktop-item1").hide();
-            $("#desktop-item2").hide();
-            $("#desktop-item3").hide();
-            $("#desktop-item4").hide();
-            $("#desktop-item5").hide();
-            $("#desktop-item6").hide();
-
-            $("#mobile-item1").hide();
-            $("#mobile-item2").hide();
-            $("#mobile-item3").hide();
-            $("#mobile-item4").hide();
-            $("#mobile-item5").hide();
-            $("#mobile-item6").hide();
-
+            $(".menu-item").hide();
             $("#btm-mainMenu").hide();
         })
 
@@ -81,21 +34,7 @@ function ItemDisplay(){
         $("#desktop-item4").click(function(){
             $("#item4Description").show();
             $("#btm-Menu").show();
-
-            $("#desktop-item1").hide();
-            $("#desktop-item2").hide();
-            $("#desktop-item3").hide();
-            $("#desktop-item4").hide();
-            $("#desktop-item5").hide();
-            $("#desktop-item6").hide();
-
-            $("#mobile-item1").hide();
-            $("#mobile-item2").hide();
-            $("#mobile-item3").hide();
-            $("#mobile-item4").hide();
-            $("#mobile-item5").hide();
-            $("#mobile-item6").hide();
-
+            $(".menu-item").hide();
             $("#btm-mainMenu").hide();
         })
 
@@ -103,21 +42,7 @@ function ItemDisplay(){
         $("#desktop-item5").click(function(){
             $("#item5Description").show();
             $("#btm-Menu").show();
-
-            $("#desktop-item1").hide();
-            $("#desktop-item2").hide();
-            $("#desktop-item3").hide();
-            $("#desktop-item4").hide();
-            $("#desktop-item5").hide();
-            $("#desktop-item6").hide();
-
-            $("#mobile-item1").hide();
-            $("#mobile-item2").hide();
-            $("#mobile-item3").hide();
-            $("#mobile-item4").hide();
-            $("#mobile-item5").hide();
-            $("#mobile-item6").hide();
-
+            $(".menu-item").hide();
             $("#btm-mainMenu").hide();
         })
 
@@ -125,21 +50,7 @@ function ItemDisplay(){
         $("#desktop-item6").click(function(){
             $("#item6Description").show();
             $("#btm-Menu").show();
-
-            $("#desktop-item1").hide();
-            $("#desktop-item2").hide();
-            $("#desktop-item3").hide();
-            $("#desktop-item4").hide();
-            $("#desktop-item5").hide();
-            $("#desktop-item6").hide();
-
-            $("#mobile-item1").hide();
-            $("#mobile-item2").hide();
-            $("#mobile-item3").hide();
-            $("#mobile-item4").hide();
-            $("#mobile-item5").hide();
-            $("#mobile-item6").hide();
-
+            $(".menu-item").hide();
             $("#btm-mainMenu").hide();
         })
 
@@ -147,21 +58,7 @@ function ItemDisplay(){
         $("#mobile-item1").click(function(){
             $("#item1Description").show();
             $("#btm-Menu").show();
-
-            $("#desktop-item1").hide();
-            $("#desktop-item2").hide();
-            $("#desktop-item3").hide();
-            $("#desktop-item4").hide();
-            $("#desktop-item5").hide();
-            $("#desktop-item6").hide();
-
-            $("#mobile-item1").hide();
-            $("#mobile-item2").hide();
-            $("#mobile-item3").hide();
-            $("#mobile-item4").hide();
-            $("#mobile-item5").hide();
-            $("#mobile-item6").hide();
-
+            $(".menu-item").hide();
             $("#btm-mainMenu").hide();
         })
 
@@ -169,21 +66,7 @@ function ItemDisplay(){
         $("#mobile-item2").click(function(){
             $("#item2Description").show();
             $("#btm-Menu").show();
-
-            $("#desktop-item1").hide();
-            $("#desktop-item2").hide();
-            $("#desktop-item3").hide();
-            $("#desktop-item4").hide();
-            $("#desktop-item5").hide();
-            $("#desktop-item6").hide();
-
-            $("#mobile-item1").hide();
-            $("#mobile-item2").hide();
-            $("#mobile-item3").hide();
-            $("#mobile-item4").hide();
-            $("#mobile-item5").hide();
-            $("#mobile-item6").hide();
-
+            $(".menu-item").hide();
             $("#btm-mainMenu").hide();
         })
 
@@ -191,21 +74,7 @@ function ItemDisplay(){
         $("#mobile-item3").click(function(){
             $("#item3Description").show();
             $("#btm-Menu").show();
-
-            $("#desktop-item1").hide();
-            $("#desktop-item2").hide();
-            $("#desktop-item3").hide();
-            $("#desktop-item4").hide();
-            $("#desktop-item5").hide();
-            $("#desktop-item6").hide();
-
-            $("#mobile-item1").hide();
-            $("#mobile-item2").hide();
-            $("#mobile-item3").hide();
-            $("#mobile-item4").hide();
-            $("#mobile-item5").hide();
-            $("#mobile-item6").hide();
-
+            $(".menu-item").hide();
             $("#btm-mainMenu").hide();
         })
 
@@ -213,21 +82,7 @@ function ItemDisplay(){
         $("#mobile-item4").click(function(){
             $("#item4Description").show();
             $("#btm-Menu").show();
-
-            $("#desktop-item1").hide();
-            $("#desktop-item2").hide();
-            $("#desktop-item3").hide();
-            $("#desktop-item4").hide();
-            $("#desktop-item5").hide();
-            $("#desktop-item6").hide();
-
-            $("#mobile-item1").hide();
-            $("#mobile-item2").hide();
-            $("#mobile-item3").hide();
-            $("#mobile-item4").hide();
-            $("#mobile-item5").hide();
-            $("#mobile-item6").hide();
-
+            $(".menu-item").hide();
             $("#btm-mainMenu").hide();
         })
 
@@ -235,21 +90,7 @@ function ItemDisplay(){
         $("#mobile-item5").click(function(){
             $("#item5Description").show();
             $("#btm-Menu").show();
-
-            $("#desktop-item1").hide();
-            $("#desktop-item2").hide();
-            $("#desktop-item3").hide();
-            $("#desktop-item4").hide();
-            $("#desktop-item5").hide();
-            $("#desktop-item6").hide();
-
-            $("#mobile-item1").hide();
-            $("#mobile-item2").hide();
-            $("#mobile-item3").hide();
-            $("#mobile-item4").hide();
-            $("#mobile-item5").hide();
-            $("#mobile-item6").hide();
-
+            $(".menu-item").hide();
             $("#btm-mainMenu").hide();
         })
 
@@ -257,49 +98,23 @@ function ItemDisplay(){
         $("#mobile-item6").click(function(){
             $("#item6Description").show();
             $("#btm-Menu").show();
-
-            $("#desktop-item1").hide();
-            $("#desktop-item2").hide();
-            $("#desktop-item3").hide();
-            $("#desktop-item4").hide();
-            $("#desktop-item5").hide();
-            $("#desktop-item6").hide();
-
-            $("#mobile-item1").hide();
-            $("#mobile-item2").hide();
-            $("#mobile-item3").hide();
-            $("#mobile-item4").hide();
-            $("#mobile-item5").hide();
-            $("#mobile-item6").hide();
-
+            $(".menu-item").hide();
             $("#btm-mainMenu").hide();
         })
 
         // BACK TO BREAKFAST MENU BUTTON
         $("#btm-Menu").click(function(){
-            $("#desktop-item1").show();
-            $("#desktop-item2").show();
-            $("#desktop-item3").show();
-            $("#desktop-item4").show();
-            $("#desktop-item5").show();
-            $("#desktop-item6").show();
 
-            $("#mobile-item1").show();
-            $("#mobile-item2").show();
-            $("#mobile-item3").show();
-            $("#mobile-item4").show();
-            $("#mobile-item5").show();
-            $("#mobile-item6").show();
+            // Display menu content
+            $(".menu-item").show();
 
+            // Display back to main menu option
             $("#btm-mainMenu").show();
 
-            $("#item1Description").hide();
-            $("#item2Description").hide();
-            $("#item3Description").hide();
-            $("#item4Description").hide();
-            $("#item5Description").hide();
-            $("#item6Description").hide();
+            // Hide Menu descriptions
+            $(".menu-description").hide();
 
+            // Hide Back to items menu button
             $("#btm-Menu").hide();
         })
 
